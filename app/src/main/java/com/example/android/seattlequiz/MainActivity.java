687971c4fb810b4511c8a74a639ef40a1419a039
erikbenton <erik.benton@hotmail.com>
2018-03-27 07:58:25 -0400

    adding basic score checking

--- app/src/main/java/com/example/android/seattlequiz/MainActivity.java
+++ app/src/main/java/com/example/android/seattlequiz/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.seattlequiz;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import com.example.android.seattlequiz.Question;
 
 
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup Q3;
     RadioGroup Q4;
     RadioGroup Q5;
-    RadioGroup[] Qs = {Q1, Q2, Q3, Q4, Q5};
+    RadioGroup[] Qs;
 
     Question q1;
     Question q2;
@@ -23,19 +26,30 @@ public class MainActivity extends AppCompatActivity {
     Question q4;
     Question q5;
 
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+    String answer5;
+
     int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initQuestions();
+        initViews();
     }
 
+    /**
+     * Initializes all the questions for loading onto the screen
+     */
     private void initQuestions()
     {
         // All the questions and answers
         String title1 = "What is the tallest building in Seattle?";
-        String answer1 = "The Columbia Center";
+        answer1 = "The Columbia Center";
         String answer11 = "Seattle Municipal Tower";
         String answer12 = "The Space Needle";
         String answer13 = "The Columbia Center";
@@ -43,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         String[] answers1 = {answer11, answer12, answer13, answer14};
 
         String title2 = "What is the body of water Seattle surrounds?";
-        String answer2 = "Puget Sound";
+        answer2 = "Puget Sound";
         String answer21 = "Puget Sound";
         String answer22 = "Duwamish Bay";
         String answer23 = "Pacific Ocean";
@@ -51,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         String[] answers2 = {answer21, answer22, answer23, answer24};
 
         String title3 = "Which teams plays for the University of Washington?";
-        String answer3 = "The Huskies";
+        answer3 = "The Huskies";
         String answer31 = "The Vikings";
         String answer32 = "The Huskies";
         String answer33 = "The Hipsters";
@@ -59,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         String[] answers3 = {answer31, answer32, answer33, answer34};
 
         String title4 = "Which beach is the most popular in West Seattle?";
-        String answer4 = "Alki Beach";
+        answer4 = "Alki Beach";
         String answer41 = "Alki Beach";
         String answer42 = "Ranier Beach";
         String answer43 = "Lincoln Park";
@@ -67,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         String[] answers4 = {answer41, answer42, answer43, answer44};
 
         String title5 = "Which tech company does NOT operate in the greater Seattle Area?";
-        String answer5 = "Apple";
+        answer5 = "Apple";
         String answer51 = "Nintendo";
         String answer52 = "Apple";
         String answer53 = "Valve";
@@ -81,6 +95,9 @@ public class MainActivity extends AppCompatActivity {
         q5 = new Question(title5, answer5, answers5);
     }
 
+    /**
+     * Initializes variables for holding views
+     */
     private void initViews()
     {
         Q1 = findViewById(R.id.question1_answers);
@@ -88,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         Q3 = findViewById(R.id.question3_answers);
         Q4 = findViewById(R.id.question4_answers);
         Q5 = findViewById(R.id.question5_answers);
-
+        Qs = new RadioGroup[] {Q1, Q2, Q3, Q4, Q5};
     }
 
     /**
@@ -104,9 +121,35 @@ public class MainActivity extends AppCompatActivity {
             RadioButton button = findViewById(Qs[i].getCheckedRadioButtonId());
             String answer = button.getText().toString();
 
-            //GET THE SCORE
+            // If the answer matches any of the actual answers
+            // then increment the score
+            if(answer.equals(answer1)
+               || answer.equals(answer2)
+               || answer.equals(answer3)
+               || answer.equals(answer4)
+               || answer.equals(answer5))
+            {
+                score++;
+            }
 
         }
 
+        // Make a toast to display their score
+        displayScore(score);
+
+    }
+
+    /**
+     * Give a toast showing the score
+     * @param score
+     */
+    private void displayScore(int score)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "You scored a " + score + "/" + Qs.length;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
